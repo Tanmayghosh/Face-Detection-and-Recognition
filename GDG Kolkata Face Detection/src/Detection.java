5a44083c2a84b9e986353369d68a42ecee1f7882
Tanmayghosh <tanmayghosh212@gmail.com>
2014-09-14 12:59:00 +0530

    Face Detection

--- GDG Kolkata Face Detection/src/Detection.java
+++ GDG Kolkata Face Detection/src/Detection.java
@@ -8,34 +8,42 @@ import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
- 
+
 public class Detection {
- 
-    public static void main(String[] args) {
- 
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("\nRunning FaceDetector");
- 
-        CascadeClassifier faceDetector = new CascadeClassifier(FaceDetector.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1));
-        Mat image = Highgui
-                .imread(Detection.class.getResource("DSCN0079.JPG").getPath().substring(1));
- 
-        MatOfRect faceDetections = new MatOfRect();
-        //faceDetector.detectMultiScale(image, faceDetections);
-      //  faceDetector.detectMultiScale(image, objects, scaleFactor, minNeighbors, flags, minSize, maxSize);
-        
-        faceDetector.detectMultiScale(image, faceDetections, 3, 2, 
-                       Objdetect.CASCADE_DO_CANNY_PRUNING , new Size(0, 0), new Size(600, 600));
- 
-        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
- 
-        for (Rect rect : faceDetections.toArray()) {
-            Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
-                    new Scalar(0, 0, 255),10);
-        }
- 
-        String filename = "ouput.png";
-        System.out.println(String.format("Writing %s", filename));
-        Highgui.imwrite(filename, image);
-    }
+
+	public static void main(String[] args) {
+
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		System.out.println("\nRunning FaceDetector");
+
+		CascadeClassifier faceDetector = new CascadeClassifier(
+				"src/Classifiers/haarcascade_frontalface_alt.xml");
+
+		// CascadeClassifier faceDetector = new
+		// CascadeClassifier(FaceDetector.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1));
+		Mat image = Highgui
+				.imread("src/Pictures/leonardo-dicaprio-golden-globes-2010-02.jpg");
+
+		MatOfRect faceDetections = new MatOfRect();
+		// faceDetector.detectMultiScale(image, faceDetections);
+		// faceDetector.detectMultiScale(image, objects, scaleFactor,
+		// minNeighbors, flags, minSize, maxSize);
+
+		faceDetector.detectMultiScale(image, faceDetections, 3, 2,
+				Objdetect.CASCADE_DO_CANNY_PRUNING, new Size(0, 0), new Size(
+						600, 600));
+
+		System.out.println(String.format("Detected %s faces",
+				faceDetections.toArray().length));
+
+		for (Rect rect : faceDetections.toArray()) {
+			Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x
+					+ rect.width, rect.y + rect.height), new Scalar(0, 0, 255),
+					10);
+		}
+
+		String filename = "ouput.png";
+		System.out.println(String.format("Writing %s", filename));
+		Highgui.imwrite(filename, image);
+	}
 }
--- GDG Kolkata Face Detection/src/FaceDetector.java
+++ GDG Kolkata Face Detection/src/FaceDetector.java
@@ -1,4 +1,3 @@
- 
 import java.io.File;
 import java.io.FilenameFilter;
 
@@ -12,75 +11,68 @@ import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
- 
+
 public class FaceDetector {
- 
-    public static void main(String[] args) {
- 
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("\nRunning FaceDetector");
- 
-        CascadeClassifier faceDetector = new CascadeClassifier(FaceDetector.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1));
-        
-        String path = "src/Pics"; 
-		 
-		  
-		  File folder = new File(path);
- 	   
-		  
-	    	 File root = new File("src/Pics");
-	         FilenameFilter pngFilter = new FilenameFilter() {
-	             public boolean accept(File dir, String name) {
-	                 return name.toLowerCase().endsWith(".png");
-	             }
-	         };
 
-	         File[] imageFiles = root.listFiles(pngFilter);
-	    	 
-	    	 
- 	 
-	     int i=0;
-	         for (File image1 : imageFiles){       
-	 
-        
-        
-i++;
-   
-System.out.print(image1.getAbsolutePath());
-//System.out.print(FaceDetector.class.getResource(image1.getAbsolutePath()));
-       Mat image = Highgui
-                .imread(image1.getAbsolutePath());
-   
-        MatOfRect faceDetections = new MatOfRect();
-        faceDetector.detectMultiScale(image, faceDetections);
- 
-        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
- int j=0;
-        for (Rect rect : faceDetections.toArray()) {
-            Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
-                    new Scalar(0, 255, 0));
-            
-          
-            
-        //    Mat imgMat = Highgui.imread(FaceDetector.class.getResource("Pics/ouput"+i+".png").getPath().substring(1), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-            
-          //  image = new Mat(imgMat, rect);  
-          image=new Mat(image,rect);
-            
-            Mat resizeimage = new Mat();
-            Size sz = new Size(300,300);
-            Imgproc.resize( image, resizeimage, sz );
-            
-            
-            String filename = "Faces/"+i+j+"-"+image1.getName();
-            System.out.println(String.format("Writing %s", filename));
-            Highgui.imwrite(filename, resizeimage);
-        
-        
-        
-        
- 
-      
-    }
+	public static void main(String[] args) {
 
-}}}
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		System.out.println("\nRunning FaceDetector");
+
+		CascadeClassifier faceDetector = new CascadeClassifier(
+				"src/Classifiers/haarcascade_frontalface_alt.xml");
+
+		String path = "src/Pictures";
+
+		
+
+		File root = new File("src/Pictures");
+		FilenameFilter pngFilter = new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".png");
+			}
+		};
+
+		File[] imageFiles = root.listFiles(pngFilter);
+
+		int i = 0;
+		for (File image1 : imageFiles) {
+
+			i++;
+
+			System.out.print(image1.getAbsolutePath());
+			// System.out.print(FaceDetector.class.getResource(image1.getAbsolutePath()));
+			Mat image = Highgui.imread(image1.getAbsolutePath());
+
+			MatOfRect faceDetections = new MatOfRect();
+			faceDetector.detectMultiScale(image, faceDetections);
+
+			System.out.println(String.format("Detected %s faces",
+					faceDetections.toArray().length));
+			int j = 0;
+			for (Rect rect : faceDetections.toArray()) {
+				Core.rectangle(image, new Point(rect.x, rect.y), new Point(
+						rect.x + rect.width, rect.y + rect.height), new Scalar(
+						0, 255, 0));
+
+				// Mat imgMat =
+				// Highgui.imread(FaceDetector.class.getResource("Pics/ouput"+i+".png").getPath().substring(1),
+				// Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+
+				// image = new Mat(imgMat, rect);
+				image = new Mat(image, rect);
+
+				Mat resizeimage = new Mat();
+				Size sz = new Size(300, 300);
+				Imgproc.resize(image, resizeimage, sz);
+
+				String filename = "Faces/" + i + j + "-" + image1.getName();
+				System.out.println(String.format("Writing %s", filename));
+				Highgui.imwrite(filename, resizeimage);
+
+			}
+
+		}
+	}
+}
